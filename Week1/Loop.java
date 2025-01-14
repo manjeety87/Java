17622ad5a2444cab33aa04ba8f8812b6fb87fc87
“manjeety87” <manjeetyadav41000@gmail.com>
2025-01-14 16:42:55 -0500

    Hellow World

---
+++ Week1/Loop.java
@@ -0,0 +1,8 @@
+public class Loop {
+    public static void main(String[] args) {
+        // This repeats the 'print' ten thousand times
+        for (int count = 1; count <= 10000; count = count + 1) {
+            System.out.print("My name is Mann! ");
+        }
+    }
+}
